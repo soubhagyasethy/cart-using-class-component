21c311a3142e3a562546c67ec0934531277eb7fd
soubhagyasethy <ssethy017@gmail.com>
2023-07-13 14:52:09 +0530

    feat: added cartItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import CartItem from "./CartItem";
+
 function App() {
   return (
     <div className="App">
-      <h1>Cart using Class components</h1>
+      <CartItem />
     </div>
   );
 }
